2d898b33b8d35d2aef665e73c13a76ce2d105f8b
NessDisk <ness.cero1@gmail.com>
2023-05-17 21:48:50 -0500

    second-part

--- Portafolio/src/app/react/react.component.ts
+++ Portafolio/src/app/react/react.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component , ViewEncapsulation} from '@angular/core';
+// import './../start/start.component.css';
 
 @Component({
   selector: 'app-react',
   templateUrl: './react.component.html',
-  styleUrls: ['./react.component.css']
+  styleUrls: ['./react.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ReactComponent {
 
